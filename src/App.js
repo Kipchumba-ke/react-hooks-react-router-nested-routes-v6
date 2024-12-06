3be66714047063533edecbbbf4cca2a663a0134b
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-12-06 22:34:40 +0300

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 function App() {
+  const[users, setUsers]=useState([])
+  useEffect(()=>{
+    fetch()
+    .then(res =>res.json())
+    .then(data => setUsers(data))
+    .catch(error =>console.error(error));
+  }, [])
   return (
     <>
       <header>
-        
+        <NavBar/>
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
